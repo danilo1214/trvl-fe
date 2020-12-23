6ea0f5c04e2f10c7a8aa8a83b068cc1ac2e4db58
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2020-12-23 19:25:45 +0100

    Updates

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,44 +1,45 @@
 <template>
-    <div class="columns" style="margin-top: 100px;">
-        <div class="column col-2 centered">
-          <g-signin-button
-            :params="googleSignInParams"
-            @success="onGoogleSignInSuccess"
-            @error="onGoogleSignInError"
-          >
-            <button class="btn btn-block btn-success">
-              Google Signin
-            </button>
-          </g-signin-button>
-        </div>
-      </div>
+  <div class="columns" style="margin-top: 100px">
+    <div class="column col-2 centered">
+      <g-signin-button
+        :params="googleSignInParams"
+        @success="onGoogleSignInSuccess"
+        @error="onGoogleSignInError"
+      >
+        <button class="btn btn-block btn-success">Google Signin</button>
+      </g-signin-button>
+    </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-    name: "Login",
-    data(){
-        return {
-            
-        }
+  name: "Login",
+  data() {
+    return {
+      googleSignInParams: {
+        client_id:
+          "720686768451-gi38hfhqsi917ll3hv59pf432to3lb0s.apps.googleusercontent.com",
+      },
+    };
+  },
+  methods: {
+    onGoogleSignInSuccess(resp) {
+      const token = resp.xc.access_token;
+      axios
+        .post("http://127.0.0.1:8000/auth/google/", {
+          access_token: token,
+        })
+        .then((resp) => {
+          this.user = resp.data.user;
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
     },
-    methods: {
-        onGoogleSignInSuccess (resp) {
-            const token = resp.xc.access_token
-            axios.post('http://127.0.0.1:8000/auth/google/', {
-                access_token: token
-            }).then(resp => {
-                this.user = resp.data.user
-            })
-            .catch(err => {
-                console.log(err.response)
-            })
-        },
-        onGoogleSignInError (error) {
-            console.log('OH NOES', error)
-        },
-        
-    }
-
-}
+    onGoogleSignInError(error) {
+      console.log("OH NOES", error);
+    },
+  },
+};
 </script>
